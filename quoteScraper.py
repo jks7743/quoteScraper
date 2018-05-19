cbb2a6b00f0734e905423ad21090d8b0bd3c0a2a
jks7743 <31747619+jks7743@users.noreply.github.com>
2018-05-18 23:20:26 -0500

    add getChamps function to quoteScraper.py delete champScraper.py for readability

--- quoteScraper.py
+++ quoteScraper.py
@@ -10,4 +10,25 @@ May 2018
 __author__ = "Joshua Schenk"
 
 import requests
+import html5lib
 from bs4 import BeautifulSoup
+
+"""
+Create a list of wiki links to all the champions listed in the league of legends
+wikia page by using BeautifulSoup4 to scrape the page
+http://leagueoflegends.wikia.com/wiki/List_of_champions
+
+:return: a list of links to champion wikia pages
+"""
+def getChamps():
+	champLinks = []
+	source = 'http://leagueoflegends.wikia.com/wiki/List_of_champions'
+	html = requests.get(source)
+	champSoup = BeautifulSoup(html.text, 'html5lib')
+	champTable = champSoup.find('table', class_='wikitable sortable')
+	champList = champTable.find("tbody")
+	for tr in champList.find_all('tr'):
+		champData = tr.find('a')
+		champLinks.append('http://leagueoflegends.wikia.com' + champData.get('href'))	# format link
+	del champLinks[0]	# delete unnecessary link
+	return champLinks
