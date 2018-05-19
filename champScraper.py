2dfb2c989931456b4f8bf52b5521d547af882f20
jks7743 <31747619+jks7743@users.noreply.github.com>
2018-05-18 22:40:58 -0500

    change getChamps function in champScraper to format the links and put them in a list to return them for use in quoteScraper

--- champScraper.py
+++ champScraper.py
@@ -1,6 +1,7 @@
 """
 Program that scrapes http://leagueoflegends.wikia.com/wiki/List_of_champions
-for a list of champion links, adds them to an array, and returns them.
+for a list of champion links, formats them, adds them to an array
+, and returns them.
 
 May 2018
 """
@@ -11,6 +12,7 @@ import html5lib
 from bs4 import BeautifulSoup
 
 def getChamps():
+	champLinks = []
 	source = 'http://leagueoflegends.wikia.com/wiki/List_of_champions'
 	html = requests.get(source)
 	champSoup = BeautifulSoup(html.text, 'html5lib')
@@ -18,7 +20,8 @@ def getChamps():
 	champList = champTable.find("tbody")
 	for tr in champList.find_all('tr'):
 		champData = tr.find('a')
-		print(champData.get('href'))
-		
+		champLinks.append('http://leagueoflegends.wikia.com' + champData.get('href'))	# format link
+	del champLinks[0]
+	return champLinks
 
-getChamps()
+print(getChamps())
