95bf0ae313abe04b3d4099257747d86be2c6181e
jks7743 <31747619+jks7743@users.noreply.github.com>
2018-05-20 15:35:31 -0500

    make get_champ_quotes take a dictionary entry instead of a champ_link, documentation for dicitonary added to get_champs

--- quote_scraper.py
+++ quote_scraper.py
@@ -18,7 +18,14 @@ Create a list of wiki links to all the champions listed in the league of legends
 wikia page by using BeautifulSoup4 to scrape the page
 http://leagueoflegends.wikia.com/wiki/List_of_champions
 
-:return: a list of links to champion wikia pages paired with the champion's name
+:return: 
+	a list of dictionaries representing champions
+		dict:
+			str Name : 'champ_name'
+			str Link : 'champ_Link'
+			str Quotes : ['"champ quote"',...]
+			str Image : 'image_link'
+			str PrettyName : 'champ name'
 """
 def get_champs():
 	champ_links = []
@@ -32,10 +39,10 @@ def get_champs():
 		champ_dict = {}
 		champ_data = tr.find('a')
 		page_link = str(champ_data.get('href'))
-		champ_dict['Name'] = page_link.rsplit('/')[2]
+		champ_dict['Name'] = page_link.rsplit('/')[2]	# get a link formated version of a champions name
 		champ_links.append(champ_dict)
 		champ_dict['Link'] = 'http://leagueoflegends.wikia.com' + page_link	# format link
-		# champ_dict['Quotes'] = get_champ_quotes(champ_dict['Link'])
+		# champ_dict['Quotes'] = get_champ_quotes(champ_dict['Link'])	# get champ quotes
 	del champ_links[0]	# delete unnecessary link
 	return champ_links
 
@@ -43,11 +50,12 @@ def get_champs():
 Function finds and returns a list of all the quotes a champion has based on a
 given wikia link
 
-:param champ_link: a link to a champions wikia page
+:param champ_dict: a dictionary entry of a champion
 :return: a list of champion quotes
 """
-def get_champ_quotes(champ_link):
+def get_champ_quotes(champ_dict):
 	champ_quotes = []
+	champ_link = champ_dict['Link']	# get a champ's link
 	html = requests.get(champ_link + '/Quotes')	# go to Quote page for easy parsing
 	soup = BeautifulSoup(html.text, 'html5lib')
 	for i in soup.find_all('i'):
