5c448e02796d872bfa70add126ffb998d138bbc0
jks7743 <31747619+jks7743@users.noreply.github.com>
2018-06-05 15:49:02 -0500

    add update_spreadsheet.py to update database for faster update times

---
+++ update_spreadsheet.py
@@ -0,0 +1,15 @@
+__author__ = "Joshua Schenk"
+
+import gspread
+import time
+from quote_scraper import get_champs
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds',
+		'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(credentials)
+
+quote_sheet = client.open("League Quotes").sheet1
+
+champ_list = get_champs()
