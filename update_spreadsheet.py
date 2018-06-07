9d24d295119cbaedee03e15f4f193f6d81ea1d63
jks7743 <31747619+jks7743@users.noreply.github.com>
2018-06-06 22:19:56 -0500

    add first version of update_spreadsheet.py

--- update_spreadsheet.py
+++ update_spreadsheet.py
@@ -1,3 +1,8 @@
+"""
+First version of update script for faster update times due to having less writes
+The script compares new data with the data in the spreadsheet to see whether a write
+is required.
+"""
 __author__ = "Joshua Schenk"
 
 import gspread
@@ -13,5 +18,21 @@ client = gspread.authorize(credentials)
 quote_sheet = client.open("League Quotes").sheet1
 
 champ_list = get_champs()
+index = 2
 
-# for champ in champ_list:
+for champ in champ_list:
+	champName = quote_sheet.cell(index, 0, value_render_option='UNFORMATTED VALUE')
+	if champName != champ['Name']:
+		row = [ champ['Name'], champ['Link'], champ['Image'], ','.join(champ['Quotes']), champ['PrettyName'] ]
+		quote_sheet.insert_row(row, index)
+		index += 1
+	else:
+		champLink = quote_sheet.cell(index, 1, value_render_option='UNFORMATTED VALUE')
+		champImage = quote_sheet.cell(index, 2, value_render_option='UNFORMATTED VALUE')
+		champQuotes = quote_sheet.cell(index, 3, value_render_option='UNFORMATTED VALUE')
+		if champ['Link'] != champLink:
+			quote_sheet.update_cell(index, 1, champ['Link'])
+		if champ['Image'] != champImage:
+			quote_sheet.update_cell(index, 1, champ['Image'])
+		if ','.join(champ['Quotes']) != champQuotes:
+			quote_sheet.update_cell(index, 1, ','.join(champ['Quotes']))
